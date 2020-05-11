dcf7f5b78eac8330b94d3edda085ace90d30ff59
InfinityMage <infinitymagebusiness@gmail.com>
2020-05-11 17:33:38 +1000

    revamped command loading

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const discord = require('discord.js');
 const client = new discord.Client();
 client.commands = new discord.Collection();
 client.config = require('./config.json');
-const auth = require('./auth.json')
+const auth = require('./auth.json');
+const NodeCache = require('node-cache');
+client.cache = new NodeCache({stdTTL: client.config.cache_time});
 
 // Load events
 fs.readdirSync('./events').forEach(file => {
@@ -12,13 +14,28 @@ fs.readdirSync('./events').forEach(file => {
     console.log(`Loaded Event: ${file}`);
 });
 
-// Load commands
-fs.readdirSync('./commands').forEach(folder => {
-    fs.readdirSync(`./commands/${folder}`).forEach(file => {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-        console.log(`Loaded Command: ${command.name}`);
+let commandFiles = [];
+let searching = true;
+let dirBuilder = ['./commands'];
+while (searching) {
+    fs.readdirSync(dirBuilder[0]).forEach(f => {
+        if (f.slice(-3) === '.js') return commandFiles.push(dirBuilder[0]+'/'+f);
+        if (!f.includes('.')) {
+            dirBuilder.push(dirBuilder[0]+'/'+f);
+        }
     });
+    dirBuilder.shift();
+    if(dirBuilder.length === 0) searching = false;
+}
+
+commandFiles.forEach(cmd => {
+    const command = require(cmd);
+    client.commands.set(command.name, command);
+    console.log(`Loaded Commands: ${command.name}`);
 });
 
 client.login(auth.DISCORD_TOKEN);
+
+module.exports = {
+    client: client
+}
