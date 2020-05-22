9afcda6f21ce65688559e2376a1d28df18f57402
InfinityMage <infinitymagebusiness@gmail.com>
2020-05-22 18:41:59 +1000

    redesigned verification command

--- commands/hypixel/verify.js
+++ commands/hypixel/verify.js
@@ -12,72 +12,48 @@ module.exports = {
     description: 'Verify your Minecraft account with your Discord account, as well as your guild membership.',
     module: 'hypixel',
     admin: false,
-    aliases: ['sync'],
-    examples: [],
+    aliases: ['link'],
+    examples: ['`verify InfinityMage` : link your discord account with the Minecraft account `InfinityMage`'],
 
     async execute (message, args, client) {
 
+        const msg = await message.channel.send('`Attempting verification...`');
         const settingsStmt = await db.prepare(`SELECT value FROM settings WHERE guild = ? AND setting = ?`);
+        const getlinkStmt = await db.prepare(`SELECT user FROM mc_linkings WHERE guild = ? AND user = ? AND minecraftuuid = ?`);
 
         if (!args[0]) {
             const guildPrefix = await settingsStmt.get(message.guild.id, 'prefix').value;
-            return message.channel.send(complexError(`Invalid usage! Please use \`${guildPrefix}${this.usage}\``));
+            return msg.edit('', {embed: complexError(`Invalid usage! Please use \`${guildPrefix}${this.usage}\``)});
         } else {
 
-            let verifiedRoleID = await settingsStmt.get(message.guild.id, 'verified_mc_role');
-            let memberRoleID = await settingsStmt.get(message.guild.id, 'guild_member_role');
-            if (memberRoleID) memberRoleID = memberRoleID.value
-
-            if (!verifiedRoleID) return message.channel.send(complexError(`There is no verified role. Please contact this Discord's administrators.`));
-            else verifiedRoleID = verifiedRoleID.value;
-
-            const verifiedRole = getRole(message.guild, verifiedRoleID);
-            if (!verifiedRole) return message.channel.send(complexError(`There is no verified role. Please contact this Discord's administrators.`));
-
-            if(message.member.roles.cache.get(verifiedRoleID) && message.member.roles.cache.get(memberRoleID)) return message.channel.send(complexError(`Your account has already been verified!`));
+            let verifiedRoleID = await settingsStmt.get(message.guild.id, 'verified_mc_role').value;
+            if (!verifiedRoleID || !getRole(message.guild, verifiedRoleID)) return message.channel.send(complexError(`There is no verified role. Please contact this Discord's administrators.`));
 
             const player_data = await getPlayer(args[0], client.cache, true);
-            if (!player_data) return message.channel.send(complexError(`An unexpected error occurred.`));
-            if (player_data.player === null) return message.channel.send(complexError(`Invalid player! Please input a valid Minecraft username.`));
+            if (!player_data) return msg.edit('', {embed: complexError(`An unexpected error occurred.`)});
+            if (player_data.player === null) return msg.edit('', {embed: complexError(`Invalid player! Please input a valid Minecraft username.`)});
 
-            if (!player_data.player.socialMedia.links.DISCORD || player_data.player.socialMedia.links.DISCORD !== message.author.tag) return message.channel.send(complexError(`Please make sure that your ingame Discord social media matches your Discord tag! This is available through the ingame profile menu.`));
+            if (!player_data.player.socialMedia.links.DISCORD || player_data.player.socialMedia.links.DISCORD !== message.author.tag) return msg.edit('', {embed: complexError(`Please make sure that your ingame Discord social media matches your Discord tag! This is available through the ingame profile menu.`)});
 
-            // give player verified role
-            else {
+            let mc_uuid = player_data.player.uuid;
 
-                message.member.roles.add(verifiedRole);
-
-                const guildID = await settingsStmt.get(message.guild.id, 'hypixel_guild').value;
-                if (guildID) {
-
-                    let check_guild_member = false;
-                    const guild_data = await getGuild(guildID, client.cache, true);
-                    guild_data.guild.members.forEach(mem => {
-                        if (mem.uuid === player_data.player.uuid) check_guild_member = true;
-                    });
-
-                    if (check_guild_member) {
-
-                        if (!memberRoleID) return;
-
-                        const memberRole = getRole(message.guild, memberRoleID);
-                        if (!memberRole) return;
-                        if(message.member.roles.cache.get(memberRoleID)) return;
-
-                        message.member.roles.add(memberRole);
-
-                    }
-
-                }
-
-                const successEmbed = new discord.MessageEmbed()
-                .setColor(client.config.color.success)
-                .setTitle(`Verified Minecraft Account`)
-                .setDescription(`:white_check_mark: Your Discord has been linked to the Minecraft account \`${player_data.player.displayname}\`.`)
-
-                message.channel.send(successEmbed)
-
+            const getLinkage = await getlinkStmt.get(message.guild.id, message.author.id, mc_uuid);
+            if (getLinkage) msg.edit('', {embed: complexError(`Your account has already been linked to \`${player_data.player.displayname}\`!`)});
+            if(!message.member.roles.cache.get(verifiedRoleID) && getLinkage) return message.member.roles.add(verifiedRoleID);
+            else if (message.member.roles.cache.get(verifiedRoleID) && getLinkage) return;
+            
+            if (!getLinkage) {
+                const insertVerifStmt = await db.prepare(`INSERT INTO mc_linkings (guild, user, minecraftuuid) VALUES (?, ?, ?)`);
+                insertVerifStmt.run(message.guild.id, message.author.id, mc_uuid);
+                message.member.roles.add(verifiedRoleID);
             }
+
+            const successEmbed = new discord.MessageEmbed()
+            .setColor(client.config.color.success)
+            .setTitle(`Verified Minecraft Account`)
+            .setDescription(`:white_check_mark: Your Discord has been linked to the Minecraft account \`${player_data.player.displayname}\`.`)
+
+            msg.edit('', {embed: successEmbed});
 
         }
 
