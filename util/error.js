65d4e9d952b24cef12cc447201b92098e91ef016
Roy Kana <41491025+0PTi0NS@users.noreply.github.com>
2020-05-23 02:24:54 -0400

    Update error.js

--- util/error.js
+++ util/error.js
@@ -9,7 +9,7 @@ module.exports = {
 
     complexError: (msg) => {
         const errorEmbed = new discord.MessageEmbed()
-        .setTitle(`Error`)
+        .setTitle(`🧊 Iceberg Error`)
         .setColor(config.color.error)
         .setDescription(msg)
 
